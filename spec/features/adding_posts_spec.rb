7d8898913b60a934414c4d328d83815a984ab3bd
NicolePell <pellicena@gmail.com>
2014-10-24 14:59:06 +0100

    took username field out of posting

--- spec/features/adding_posts_spec.rb
+++ spec/features/adding_posts_spec.rb
@@ -4,17 +4,16 @@ feature "User adds a new link" do
 	scenario "when browsing the homepage" do
 		expect(Post.count).to eq(0)
 		visit '/'
-		add_post('@nic', 'AN EVEN BETTER POST THAN THE BEST POST')
+		add_post('AN EVEN BETTER POST THAN THE BEST POST')
 		expect(Post.count).to eq(1)
 		post = Post.first
 		expect(post.post).to eq('AN EVEN BETTER POST THAN THE BEST POST')
-		expect(post.user_name).to eq('@nic')
+		
 	end
 
-	def add_post(user_name, post)
+	def add_post(post)
 		within('#new-post') do
 			fill_in 'post', with: post
-			fill_in 'user_name', with: user_name
 			click_button 'Post'
 		end
 	end
